b2c88d648d1c2f5800f301796683ecdd1d56fda6
voter7 <hazieva-alina92@yandex.ru>
2021-02-05 15:14:04 +0300

    remove comments

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -220,13 +220,13 @@ window.addEventListener('DOMContentLoaded', () => {
 
     const animItems = document.querySelectorAll('.animate-items');
 
-    if (animItems.length > 0) {                    //Проверяем есть-ли элементы на странице
+    if (animItems.length > 0) {                    
         window.addEventListener('scroll', animOnScroll);
         function animOnScroll() {
             for (let i = 0; i < animItems.length; i++) {
                 const animItem = animItems[i];
-                const animItemHeight = animItem.offsetHeight;   //получаем высоту каждого элемента
-                const animItemOffset = offset(animItem).top;   //сколько проскролено сверху
+                const animItemHeight = animItem.offsetHeight;  
+                const animItemOffset = offset(animItem).top;  
                 const animStart = 4;
 
                 let animItemPoint = window.innerHeight - animItemHeight / animStart;   //высота окна минус 1/4 высоты элемента
@@ -248,8 +248,8 @@ window.addEventListener('DOMContentLoaded', () => {
 
         function offset(el) {
             const rect = el.getBoundingClientRect(); //возвращает размер элемента и его позицию относительно viewport (часть страницы, показанная на экране, и которую мы видим).
-            let scrollLeft = window.pageXOffset || document.documentElement.scrollLeft;             //текущее состояние прокрутки по ширине.
-            let scrollTop = window.pageYOffset || document.documentElement.scrollTop;               //текущее состояние прокрутки по высоте.
+            let scrollLeft = window.pageXOffset || document.documentElement.scrollLeft;             
+            let scrollTop = window.pageYOffset || document.documentElement.scrollTop;              
             return { top: rect.top + scrollTop, left: rect.left + scrollLeft};
 
         }
@@ -260,47 +260,6 @@ window.addEventListener('DOMContentLoaded', () => {
 
         
     }
-
-    
-    // const animItems = document.querySelectorAll(`.anim-items`);
-    //     if (animItems.length > 0) {
-    //         window.addEventListener(`scroll`, animOnScroll);
-
-    //         function animOnScroll() {
-    //             for (let index = 0; index < animItems.length; index++) {
-    //                 const animItem = animItems[index];
-    //                 const animItemHeight = animItem.offsetHeight;
-    //                 const animItemOffSet = offset(animItem).top;
-    //                 const animStart = 4;
-    //                 let animItemPoint = window.innerHeight - animItemHeight / animStart;
-    //                 if (animItemHeight > window.innerHeight) {
-    //                     animItemPoint = window.innerHeight - window.innerHeight / animStart;
-    //                 }
-    //                 if ((pageYOffset > animItemOffSet - animItemPoint) && pageYOffset < (animItemOffSet + animItemHeight)) {
-    //                     animItem.classList.add(`active`);
-    //                 } else {
-    //                     // if (!(animItem.classList.contains(`_anim-no-hide`))) {
-                           
-    //                     // }
-    //                      animItem.classList.remove(`active`);
-    //                 }
-    //             }
-    //         }
-
-    //         function offset(el) {
-    //             const rect = el.getBoundingClientRect()
-    //             let scrollLeft = window.pageXOffset || document.documentElement.scrollLeft;
-    //             let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-    //             return {top: rect.top + scrollTop, left: rect.left + scrollLeft};
-    //         }
-
-    // // setTimeout(() => {
-    // //     animOnScroll()
-    // // }, 300)
-    // }
-        
-
-
 
 
 });
